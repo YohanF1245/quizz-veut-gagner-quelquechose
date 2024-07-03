e736a85e7cdbbc25358163385d234f72a49addaf
YohanF1245 <yohan.ferdin@hotmail.fr>
2024-07-03 16:37:30 +0200

    working on select category

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { useState, useSyncExternalStore } from 'react'
-import dataeasy from './question-disney-facile.json'
-import datad from './question-disney-moyen.json'
+import disneyEasy from './question-disney-facile.json'
+import disneyMedium from './question-disney-moyen.json'
+import disneyHard from './question-disney-difficile.json'
+import cinema from './question-cinema.json'
+import harryPotter from './question-harry-potter.json'
+import questions from './questions.json'
 import './title.css';
-import data from './question-disney-difficile.json'
+import './modal-end.css';
 import backgroundimage from './qui-veut-gagner-des-millions.jpg'
 
 function App() {
-  const [questionId, setQuestionId] = useState(randomQuestion());
+  const datas = [
+    { value: 'disneyEasy', label: "Disney facile"},
+    { value: 'disneyMedium', label: "Disney medium"},
+    { value: 'disneyHard', label: "Disney hard"},
+    { value: 'cinema', label: "Cinema"},
+    { value: 'harryPotter', label: "Harry potter"}
+  ];
+  const [selectedTheme, setSelectedTheme] = useState("Cinema");
+  
+  console.log(questions.categories.find(category => category.name === selectedTheme));
+  const [questionId, setQuestionId] = useState(randomQuestion(questions.categories.find(category => category.name === selectedTheme).questions));
   const [winDisplay, setWinDisplay] = useState('none');
   const [loseDisplay, setLoseDisplay] = useState('none');
   const [nextQuestion, setNextQuestion] = useState('none');
   const [endDisplay, setEndDisplay] = useState('none');
-  
 
   const fontStyle = {
     textShadow: '0 0 9px #000000, 0 0 9px #000000',
@@ -32,7 +45,7 @@ function App() {
   const styleBarButt = {
     backgroundImage: 'linear-gradient(to top, #1600ff, #6547ff, #8f73ff, #b39dff, #d3c7ff, #d3c7ff, #d3c7ff, #d3c7ff, #b39dff, #8f73ff, #6547ff, #1600ff)',
     height:'75px',
-    width: '20%',
+    width: '30%',
     border: "5px solid white",
     boxShadow: '0px 10px 0px 0px #000000, 0px -10px 0px 0px #000000',
     borderLeft: "0px transparent",
@@ -70,21 +83,26 @@ function App() {
     borderRadius: '0% 100% 100% 0%',
   }
   const styleEnd = {
+    display:"flex",
+    flexDirection:"column",
+    jutifyContent:"center",
+    alignItems:"center",
     width:"30%",
     height:'30%',
    borderRadius: "35px",
    position:"absolute",
    left:"50%",
+   border:"10px solid black",
    transform: "translate(-50%,0)",
     display:"block",
-    backgroundColor: "white",
+    backgroundColor: "rgba(255,255,255,0.5)",
   }
 
   const styleHexLeftButt = {
     backgroundImage: 'linear-gradient(to top, #1600ff, #6547ff, #8f73ff, #b39dff, #d3c7ff, #d3c7ff, #d3c7ff, #d3c7ff, #b39dff, #8f73ff, #6547ff, #1600ff)',
     
     height:'75px',
-    width: '10%',
+    width: '5%',
     border: "5px solid white",
     boxShadow: '0px 10px 0px 0px #000000, 0px -10px 0px 0px #000000',
     borderLeft: "0px transparent",
@@ -94,17 +112,21 @@ function App() {
   const styleHexRightButt = {
     backgroundImage: 'linear-gradient(to top, #1600ff, #6547ff, #8f73ff, #b39dff, #d3c7ff, #d3c7ff, #d3c7ff, #d3c7ff, #b39dff, #8f73ff, #6547ff, #1600ff)',
     minHeight: '75px',
-    width: '10%',
+    width: '5%',
     border: "5px solid white",
     boxShadow: '0px 10px 0px 0px #000000, 0px -10px 0px 0px #000000',
     borderLeft: "0px transparent",
     borderRight: "0px transparent",
     borderRadius: '0% 100% 100% 0%',
   }
+  const handleSelect = (event) => {
+    const selectedValue = event.target.value;
+   setSelectedTheme(selectedValue);
+  }
   const handleClick = (event) => {
-    setEndDisplay("block");
+    setEndDisplay("flex");
     setNextQuestion("block");
-    if(event.target.value===data[questionId].correct_answer){
+    if(event.target.value===questions.categories.find(category => category.name === selectedTheme).questions[questionId].correct_answer){
     setWinDisplay("block");
     setLoseDisplay("none");
     }else{
@@ -113,7 +135,7 @@ function App() {
     }
   }
   const nextQuestionButton = (event) => {
-    setQuestionId(randomQuestion());
+    setQuestionId(randomQuestion(questions.categories.find(category => category.name === selectedTheme).questions));
     setWinDisplay("none");
     setLoseDisplay("none");
     setNextQuestion("none");
@@ -128,14 +150,27 @@ function App() {
     <div className="text-effect-wrapper , sizeMatters">
       
     <Title></Title>
+    
     </div>
+    <div style={{backgroundColor: "white", width: "30%", marginLeft:"35%", textAlign:"center"}}>
+      <label htmlFor="theme">Choisissez un theme</label>
+      <select onChange={handleSelect} name="theme" id="theme"> 
+      <option value="">Sélectionnez un thème</option>
+      {datas.map((item) => (
+          <option key={item.value} value={item.label}>
+            {item.label}
+          </option>
+        ))}
+      </select>
+    </div>
+
     {/* <img src={backgroundimage} style={{width:"50%", marginLeft:"25%"}}></img> */}
     <div style={{position: "absolute", bottom:"0",width:"100%"}}>
     <div style={{ display:'flex', alignItems:'center'}}>
     <div style={styleMiniBar}></div>
     <div style={styleHexLeft}></div>
     <div style={styleBar}>
-    <p style={fontStyle}>{data[questionId].question}</p></div>
+    <p style={fontStyle}>{questions.categories.find(category => category.name === selectedTheme).questions[questionId].question}</p></div>
     <div style={styleHexRight}></div>
     
     <div style={styleMiniBar}></div>
@@ -143,10 +178,10 @@ function App() {
     <div style={{marginTop:"25px", display:'flex', alignItems:'center'}}>
       <div style={styleMiniBar}></div>
       <div style={styleHexLeftButt}></div>
-    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="1" value={data[questionId].choices[0]}></input>
+    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="1" value={questions.categories.find(category => category.name === selectedTheme).questions[questionId].choices[0]}></input>
     <div style={styleHexRightButt}></div>
     <div style={styleHexLeftButt}></div>
-    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="2" value={data[questionId].choices[1]}></input>
+    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="2" value={questions.categories.find(category => category.name === selectedTheme).questions[questionId].choices[1]}></input>
     <div style={styleHexRightButt}></div>
     <div style={styleMiniBar}></div>
     </div>
@@ -154,30 +189,27 @@ function App() {
     <div style={{marginTop:"25px", display:'flex', alignItems:'center'}}>
       <div style={styleMiniBar}></div>
       <div style={styleHexLeftButt}></div>
-    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="3" value={data[questionId].choices[2]}></input>
+    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="3" value={questions.categories.find(category => category.name === selectedTheme).questions[questionId].choices[2]}></input>
     <div style={styleHexRightButt}></div>
     <div style={styleHexLeftButt}></div>
-    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="4" value={data[questionId].choices[3]}></input>
+    <input style={{...styleBarButt,...fontStyle}} onClick={handleClick} type="button" id="4" value={questions.categories.find(category => category.name === selectedTheme).questions[questionId].choices[3]}></input>
     <div style={styleHexRightButt}></div>
     <div style={styleMiniBar}></div>
     </div>
     </div>
     
     <div style={combinedStyleEnd}>
-    <p style={{ display: winDisplay }}>Gagné ! En effet la bonne réponse est : {data[questionId].correct_answer}</p>
-    <p style={{ display: loseDisplay }}>Perdu ! La bonne réponse était : {data[questionId].correct_answer}</p>
-    <input style={{ display: nextQuestion }} onClick={nextQuestionButton} type="button" value="Question suivante"></input>
+    <p className="text-end-game" style={{ display: winDisplay }}>Gagné ! En effet la bonne réponse est : {questions.categories.find(category => category.name === selectedTheme).questions[questionId].correct_answer}</p>
+    <p className="text-end-game" style={{ display: loseDisplay }}>Perdu ! La bonne réponse était : {questions.categories.find(category => category.name === selectedTheme).questions[questionId].correct_answer}</p>
+    <input className="button-64" style={{ display: nextQuestion }} onClick={nextQuestionButton} type="button" value="Question suivante"></input>
     </div>
     </div>
 )
 }
-function randomQuestion(){
+function randomQuestion(data){
   return Math.floor(Math.random() * data.length)
 }
 
-data.map(question => {
-  console.log(question.question)
-});
 function Title(){
   return(<h1 className="text">Quizz veux gagner quelquechose</h1>)
 }
